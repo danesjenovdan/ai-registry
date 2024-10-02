f02171eaeaebff12c370595677ab1993efa7687a
LepkoQQ <lepko.san@gmail.com>
2024-10-02 18:34:36 +0200

    rename project to ai_registry

--- ai_registry/ai_registry/settings/k8s.py
+++ ai_registry/ai_registry/settings/k8s.py
@@ -4,5 +4,5 @@ DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "<TODO>")
 
-ALLOWED_HOSTS = ["ai-register.lb.djnd.si"]
-CSRF_TRUSTED_ORIGINS = ["https://ai-register.lb.djnd.si"]
+ALLOWED_HOSTS = ["ai-registry.lb.djnd.si"]
+CSRF_TRUSTED_ORIGINS = ["https://ai-registry.lb.djnd.si"]
--- ai_registry/ai_registry/wsgi.py
+++ ai_registry/ai_registry/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ai_register project.
+WSGI config for ai_registry project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_register.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ai_registry.settings.dev')
 
 application = get_wsgi_application()
