9237433e03d7ead40e3caab259024b76a046c8c1
LepkoQQ <lepko.san@gmail.com>
2024-10-11 00:01:01 +0200

    implement pagination

--- ai_registry/home/views.py
+++ ai_registry/home/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 
 from .models import RegistryEntry
@@ -8,5 +9,17 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["entries"] = RegistryEntry.objects.all()
+
+        entries = RegistryEntry.objects.all()
+        paginator = Paginator(entries, 10)
+        page_query = self.request.GET.get("page", 1)
+        page_obj = paginator.get_page(page_query)
+        paginator.elided_page_range = paginator.get_elided_page_range(
+            number=page_obj.number,
+            on_each_side=3,
+            on_ends=0,
+        )
+
+        context["entries"] = page_obj
+
         return context
