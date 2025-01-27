4d5936b0f8061d33e31d593e8ac47b28b2900164
LepkoQQ <lepko.san@gmail.com>
2025-01-27 16:50:10 +0100

    add csv export

--- ai_registry/home/urls.py
+++ ai_registry/home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import CSVExportView, HomeView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home_view"),
+    path("export/data.csv", CSVExportView.as_view(), name="csv_export"),
 ]
--- ai_registry/home/views.py
+++ ai_registry/home/views.py
@@ -1,7 +1,10 @@
+import csv
+
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 
 from .models.registry_entry import RegistryEntry
 from .models.tags import AreaTag, GenericTag, InstitutionTag
@@ -100,3 +103,73 @@ class HomeView(TemplateView):
         context["search"] = search
 
         return context
+
+
+class CSVExportView(View):
+    def get(self, request, *args, **kwargs):
+        file_name = _("izvoz-register-ui")
+        latest_updated_entry = RegistryEntry.objects.order_by("-updated_at").first()
+        if latest_updated_entry:
+            file_name += (
+                f"_{latest_updated_entry.updated_at.strftime('%Y-%m-%d_%H-%M-%S')}"
+            )
+
+        response = HttpResponse(
+            content_type="text/csv",
+            headers={"Content-Disposition": f'attachment; filename="{file_name}.csv"'},
+        )
+
+        writer = csv.writer(response)
+        writer.writerow(
+            [
+                _("Ime orodja"),
+                _("Namen orodja"),
+                _("Opis"),
+                _("Obdobje rabe"),
+                _("Institucije"),
+                _("Področja"),
+                _("Oznake"),
+                _("Razvijalci"),
+                _("Cena"),
+                _("Komentarji o ceni"),
+                _("Trajanje licence (če kupljeno)"),
+                _("Komentarji o trajanju licence"),
+                _("Analiza učinka na človekove pravice opravljena"),
+                _("Komentarji analize učinka na človekove pravice"),
+                _("Analiza učinka na osebne podatke opravljena"),
+                _("Komentarji analize učinka na osebne podatke"),
+                _("Povezave"),
+                _("Posodobljeno"),
+            ]
+        )
+
+        entries = RegistryEntry.objects.prefetch_related("links").all()
+        for entry in entries:
+            links = [
+                f"{link.description}: {link.url or link.file}"
+                for link in entry.links.all()
+            ]
+            writer.writerow(
+                [
+                    entry.name,
+                    entry.purpose,
+                    entry.description,
+                    entry.time_in_use,
+                    "; ".join(str(i) for i in entry.institutions.all()),
+                    "; ".join(str(a) for a in entry.areas.all()),
+                    "; ".join(str(t) for t in entry.tags.all()),
+                    entry.developers,
+                    entry.cost,
+                    entry.cost_comment,
+                    entry.license_duration,
+                    entry.license_duration_comment,
+                    _("Da") if entry.human_rights_analysis_done else _("Ne"),
+                    entry.human_rights_analysis_comments,
+                    _("Da") if entry.personal_data_analysis_done else _("Ne"),
+                    entry.personal_data_analysis_comments,
+                    "; ".join(links),
+                    entry.updated_at.strftime("%Y-%m-%dT%H:%M:%S"),
+                ]
+            )
+
+        return response
