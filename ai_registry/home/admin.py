0f7147ee3a849b382a7ae4da9c39bf5b7fa55b7e
LepkoQQ <lepko.san@gmail.com>
2025-11-11 12:28:28 +0100

    remove old fields and clean up

--- ai_registry/home/admin.py
+++ ai_registry/home/admin.py
@@ -9,69 +9,89 @@ from .models.tags import AreaTag, GenericTag, InstitutionTag
 admin.site.unregister(Tag)
 
 
-class LinkAdmin(admin.TabularInline):
+class LinkAdminInline(admin.TabularInline):
     model = Link
     extra = 0
 
 
-class RegistryEntryInstitutionDataAdmin(admin.StackedInline):
+REGISTRY_ENTRY_INSTITUTION_DATA_ADMIN_FIELDSETS_INLINE = [
+    (
+        None,
+        {
+            "fields": [
+                "institution",
+            ]
+        },
+    ),
+    (
+        _("CENA"),
+        {
+            "fields": [
+                "cost",
+                "cost_comment",
+            ]
+        },
+    ),
+    (
+        _("LICENCA"),
+        {
+            "fields": [
+                "license_duration",
+                "license_duration_comment",
+            ]
+        },
+    ),
+    (
+        _("ANALIZE"),
+        {
+            "fields": [
+                "human_rights_analysis_done",
+                "human_rights_analysis_comments",
+                "personal_data_analysis_done",
+                "personal_data_analysis_comments",
+            ]
+        },
+    ),
+    (
+        _("JAVNO NAROČILO"),
+        {
+            "fields": [
+                "public_procurement",
+                "public_procurement_number",
+                "public_procurement_number_eu",
+                "public_procurement_date",
+                "contracting_institution",
+            ]
+        },
+    ),
+]
+
+REGISTRY_ENTRY_INSTITUTION_DATA_ADMIN_FIELDSETS = [
+    (
+        None,
+        {
+            "fields": [
+                "registry_entry",
+                "institution",
+            ]
+        },
+    ),
+    *REGISTRY_ENTRY_INSTITUTION_DATA_ADMIN_FIELDSETS_INLINE[1:],
+]
+
+
+class RegistryEntryInstitutionDataAdminInline(admin.StackedInline):
     model = RegistryEntryInstitutionData
     extra = 0
-    fieldsets = [
-        (
-            None,
-            {
-                "fields": [
-                    "institution",
-                ]
-            },
-        ),
-        (
-            _("CENA"),
-            {
-                "fields": [
-                    "cost",
-                    "cost_comment",
-                ]
-            },
-        ),
-        (
-            _("LICENCA"),
-            {
-                "fields": [
-                    "license_duration",
-                    "license_duration_comment",
-                ]
-            },
-        ),
-        (
-            _("ANALIZE"),
-            {
-                "fields": [
-                    "human_rights_analysis_done",
-                    "human_rights_analysis_comments",
-                    "personal_data_analysis_done",
-                    "personal_data_analysis_comments",
-                ]
-            },
-        ),
-        (
-            _("JAVNO NAROČILO"),
-            {
-                "fields": [
-                    "public_procurement",
-                    "public_procurement_number",
-                    "public_procurement_number_eu",
-                    "public_procurement_date",
-                    "contracting_institution",
-                ]
-            },
-        ),
-    ]
+    fieldsets = REGISTRY_ENTRY_INSTITUTION_DATA_ADMIN_FIELDSETS_INLINE
+
+
+class RegistryEntryInstitutionDataAdmin(admin.ModelAdmin):
+    fieldsets = REGISTRY_ENTRY_INSTITUTION_DATA_ADMIN_FIELDSETS
 
 
 class RegistryEntryAdmin(admin.ModelAdmin):
-    inlines = [RegistryEntryInstitutionDataAdmin, LinkAdmin]
+    inlines = [RegistryEntryInstitutionDataAdminInline, LinkAdminInline]
     fieldsets = [
         (
             None,
@@ -80,7 +100,6 @@ class RegistryEntryAdmin(admin.ModelAdmin):
                     "name",
                     "purpose",
                     "description",
-                    "institutions",
                     "areas",
                     "tags",
                     "developers",
@@ -109,6 +128,8 @@ class RegistryEntryAdmin(admin.ModelAdmin):
 
 
 admin.site.register(RegistryEntry, RegistryEntryAdmin)
+admin.site.register(RegistryEntryInstitutionData, RegistryEntryInstitutionDataAdmin)
+admin.site.register(Link)
 admin.site.register(GenericTag)
 admin.site.register(AreaTag)
 admin.site.register(InstitutionTag)
--- ai_registry/home/models/registry_entry.py
+++ ai_registry/home/models/registry_entry.py
@@ -3,9 +3,21 @@ from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
 
 from .base import Timestamped
+from .tags import custom_tag_string_joiner
 
 
 class Link(models.Model):
+    # related registry entry
+    registry_entry = models.ForeignKey(
+        "RegistryEntry",
+        on_delete=models.CASCADE,
+        related_name="links",
+    )
+    # general fields
+    description = models.CharField(
+        max_length=255,
+        verbose_name=_("Opis povezave"),
+    )
     url = models.URLField(
         null=True,
         blank=True,
@@ -16,16 +28,6 @@ class Link(models.Model):
         blank=True,
         verbose_name=_("Datoteka"),
     )
-    description = models.CharField(
-        max_length=255,
-        verbose_name=_("Opis povezave"),
-    )
-    # related registry entry
-    registry_entry = models.ForeignKey(
-        "RegistryEntry",
-        on_delete=models.CASCADE,
-        related_name="links",
-    )
 
     def __str__(self):
         return self.description
@@ -35,142 +37,14 @@ class Link(models.Model):
         verbose_name_plural = _("Povezave")
 
 
-class RegistryEntry(Timestamped):
-    published = models.BooleanField(
-        default=True,
-        verbose_name=_("Objavljeno"),
-        help_text=_("Ali je vnos v register objavljen?"),
-    )
-    name = models.CharField(max_length=100, verbose_name=_("Ime orodja"))
-    purpose = models.CharField(max_length=255, verbose_name=_("Namen orodja"))
-    description = models.TextField(
-        blank=True,
-        verbose_name=_("Opis"),
-        help_text=_("Kakšne odločitve se sprejemajo, kaj dela ta algoritem"),
-    )
-    # links = Related
-    #   Povezava do pridobljenih materialov / viri
-    time_in_use = models.CharField(
-        max_length=30,
-        blank=True,
-        verbose_name=_("Obdobje rabe (uporabljeno za prikaz)"),
-    )
-    time_in_use_date = models.DateField(
-        null=True,
-        blank=True,
-        verbose_name=_("Obdobje rabe (uporabljeno za razvrščanje)"),
-    )
-    institutions = TaggableManager(
-        through="home.TaggedInstitution",
-        blank=True,
-        verbose_name=_("Institucije"),
-        help_text=_("Ločene s podpičjem"),
-        related_name="registryentry",
-    )
-    areas = TaggableManager(
-        through="home.TaggedArea",
-        blank=True,
-        verbose_name=_("Področja"),
-        help_text=_("Ločena s podpičjem"),
-    )
-    tags = TaggableManager(
-        through="home.TaggedGeneric",
-        blank=True,
-        verbose_name=_("Oznake"),
-        help_text=_("Ločene s podpičjem"),
-    )
-    developers = models.CharField(
-        max_length=255,
-        blank=True,
-        verbose_name=_("Razvijalci"),
-    )
-    cost = models.CharField(
-        max_length=50,
-        blank=True,
-        verbose_name=_("Cena"),
-    )
-    cost_comment = models.TextField(
-        blank=True,
-        verbose_name=_("Komentarji o ceni"),
-    )
-    license_duration = models.CharField(
-        max_length=50,
-        blank=True,
-        verbose_name=_("Trajanje licence (če kupljeno)"),
-    )
-    license_duration_comment = models.TextField(
-        blank=True,
-        verbose_name=_("Komentarji o trajanju licence"),
-    )
-    human_rights_analysis_done = models.BooleanField(
-        default=False,
-        verbose_name=_("Analiza učinka na človekove pravice opravljena"),
-    )
-    human_rights_analysis_comments = models.TextField(
-        blank=True,
-        verbose_name=_("Komentarji analize učinka na človekove pravice"),
-    )
-    personal_data_analysis_done = models.BooleanField(
-        default=False,
-        verbose_name=_("Analiza učinka na osebne podatke opravljena"),
-    )
-    personal_data_analysis_comments = models.TextField(
-        blank=True,
-        verbose_name=_("Komentarji analize učinka na osebne podatke"),
-    )
-    comments = models.TextField(
-        blank=True,
-        verbose_name=_("Dodatni komentarji"),
-        help_text=_("Interno"),
-    )
-    # javna naročila
-    public_procurement = models.BooleanField(
-        default=False,
-        verbose_name=_("Javno naročilo"),
-        help_text=_("Ali je bil vnos predmet javnega naročila?"),
-    )
-    public_procurement_number = models.CharField(
-        max_length=50,
-        null=True,
-        blank=True,
-        verbose_name=_("Številka objave na PJN"),
-        help_text=_(
-            "Številka javnega naročila na portalu javnih naročil (PJN), če je predmet javnega naročila"
-        ),
-    )
-    public_procurement_number_eu = models.CharField(
-        max_length=50,
-        null=True,
-        blank=True,
-        verbose_name=_("Številka objave na TED"),
-        help_text=_(
-            "Številka javnega naročila na portalu TED (tenders electronic daily), če je predmet javnega naročila"
-        ),
-    )
-    public_procurement_date = models.DateField(
-        null=True,
-        blank=True,
-        verbose_name=_("Datum objave javnega naročila"),
-        help_text=_("Datum objave javnega naročila, če je predmet javnega naročila"),
-    )
-    contracting_institution = TaggableManager(
-        through="home.TaggedContractingInstitution",
-        blank=True,
-        related_name="contract_registryentry",
-        verbose_name=_("Naročnik"),
-        help_text=_("Ločene s podpičjem"),
-    )
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = _("Vnos v register")
-        verbose_name_plural = _("Vnosi v register")
-        ordering = ["-created_at"]
-
-
 class RegistryEntryInstitutionData(Timestamped):
+    # related registry entry
+    registry_entry = models.ForeignKey(
+        "RegistryEntry",
+        on_delete=models.CASCADE,
+        related_name="registryentryinstitutiondata",
+    )
+    # general fields
     institution = TaggableManager(
         through="home.TaggedInstitution",
         blank=True,
@@ -252,20 +126,68 @@ class RegistryEntryInstitutionData(Timestamped):
         verbose_name=_("Naročnik"),
         help_text="",
     )
-    # related registry entry
-    registry_entry = models.ForeignKey(
-        "RegistryEntry",
-        on_delete=models.CASCADE,
-        related_name="registryentryinstitutiondata",
+
+    def __str__(self):
+        institution = custom_tag_string_joiner(self.institution.all())
+        return f"{self.registry_entry} - {institution}"
+
+    class Meta:
+        verbose_name = _("Podatki institucije pri vnosu")
+        verbose_name_plural = _("Podatki institucij pri vnosih")
+
+
+class RegistryEntry(Timestamped):
+    name = models.CharField(max_length=100, verbose_name=_("Ime orodja"))
+    purpose = models.CharField(max_length=255, verbose_name=_("Namen orodja"))
+    description = models.TextField(
+        blank=True,
+        verbose_name=_("Opis"),
+        help_text=_("Kakšne odločitve se sprejemajo, kaj dela ta algoritem"),
+    )
+    areas = TaggableManager(
+        through="home.TaggedArea",
+        blank=True,
+        verbose_name=_("Področja"),
+        help_text=_("Ločena s podpičjem"),
+    )
+    tags = TaggableManager(
+        through="home.TaggedGeneric",
+        blank=True,
+        verbose_name=_("Oznake"),
+        help_text=_("Ločene s podpičjem"),
+    )
+    developers = models.CharField(
+        max_length=255,
+        blank=True,
+        verbose_name=_("Razvijalci"),
+    )
+    published = models.BooleanField(
+        default=True,
+        verbose_name=_("Objavljeno"),
+        help_text=_("Ali je vnos v register objavljen?"),
+    )
+    # time in use
+    time_in_use = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name=_("Obdobje rabe (uporabljeno za prikaz)"),
+    )
+    time_in_use_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name=_("Obdobje rabe (uporabljeno za razvrščanje)"),
+    )
+    # internal
+    comments = models.TextField(
+        blank=True,
+        verbose_name=_("Dodatni komentarji"),
+        help_text=_("Interno"),
     )
 
     def __str__(self):
-        return f"{self.registry_entry} - {self.institution}"
+        return self.name
 
     class Meta:
-        verbose_name = _(
-            "Podatki povezani s specifično institucijo pri vnosu v register"
-        )
-        verbose_name_plural = _(
-            "Podatki povezani s specifičnimi institucijami pri vnosih v register"
-        )
+        verbose_name = _("Vnos v register")
+        verbose_name_plural = _("Vnosi v register")
+        ordering = ["-created_at"]
